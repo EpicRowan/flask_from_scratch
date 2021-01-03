b8a3abac30424f79db26ad911e0a73424e29749c
Rowan <shepherd.rowan.k@gmail.com>
2021-01-02 19:50:45 -0800

    Added render template from flask

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def home:
+	return render_template("homepage.html")
+
 
 if __name__ == 'main':
 	app.run(debug=True)
